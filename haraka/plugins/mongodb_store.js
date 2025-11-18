dc25bddcca21c805610906e9e07910d1d57a8965
Arslan Malik <arsalmalik1920@gmail.com>
2025-11-18 21:48:17 +0400

    --

--- haraka/plugins/mongodb_store.js
+++ haraka/plugins/mongodb_store.js
@@ -9,6 +9,8 @@ exports.register = function () {
   const dbName = process.env.MONGO_DB || "tempmail";
   const collName = process.env.MONGO_COLLECTION || "emails";
 
+  plugin.loginfo("PLUGIN REGISTER STARTED");
+
   MongoClient.connect(url)
     .then((client) => {
       const db = client.db(dbName);
@@ -19,11 +21,16 @@ exports.register = function () {
       plugin.logerror("Mongo connection error: " + err.message);
     });
 
+  // Add more logs here
+  plugin.loginfo("Registering hook: data_post");
+
   this.register_hook("data_post", "save_to_mongo");
 };
 
 exports.save_to_mongo = function (next, connection) {
   const plugin = this;
+
+  plugin.loginfo("---- DATA_POST HOOK TRIGGERED ----");
 
   if (!collection) {
     plugin.logerror("Mongo not ready, skipping save");
@@ -32,35 +39,55 @@ exports.save_to_mongo = function (next, connection) {
 
   const txn = connection.transaction;
 
+  plugin.loginfo("Transaction UUID: " + txn.uuid);
+
   const mail_from = txn.mail_from && txn.mail_from.address();
   const rcpt_to = txn.rcpt_to && txn.rcpt_to.map((addr) => addr.address());
-  const subject = txn.header ? txn.header.get("subject") || "" : "";
-  const headers = txn.header ? txn.header.headers_decoded : {};
 
-  // Haraka message_stream.get_data gives only "data", no error param
+  plugin.loginfo("Mail From: " + mail_from);
+  plugin.loginfo("RCPT TO: " + JSON.stringify(rcpt_to));
+
+  const subject = txn.header ? txn.header.get("subject") || "" : "";
+  plugin.loginfo("Subject: " + subject);
+
+  // Log how many times get_data fires
+  let getDataCount = 0;
+
+  plugin.loginfo("Calling message_stream.get_data...");
+
   txn.message_stream.get_data((data) => {
+    getDataCount++;
+    plugin.loginfo("get_data fired: " + getDataCount + " time(s)");
+
     try {
       const body = data.toString("utf8");
+
+      plugin.loginfo("Body length: " + body.length);
 
       const doc = {
         mail_from,
         rcpt_to,
         subject,
-        headers,
+        headers: txn.header ? txn.header.headers_decoded : {},
         body,
         receivedAt: new Date(),
+        debug_uuid: txn.uuid,
+        debug_get_data_call: getDataCount
       };
+
+      plugin.loginfo("Attempting Mongo insertOne...");
 
       collection
         .insertOne(doc)
         .then(() => {
-          plugin.loginfo("Saved email for " + rcpt_to);
+          plugin.loginfo("Inserted email for " + rcpt_to + " with UUID " + txn.uuid);
           next();
         })
         .catch((e) => {
           plugin.logerror("Mongo insert error: " + e.message);
           next();
         });
+
     } catch (e) {
       plugin.logerror("Exception reading message stream: " + e.message);
       next();
