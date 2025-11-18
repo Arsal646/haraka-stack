d80540a70549be7ecb5c7ed8393d8bbfcfcef00c
Arslan Malik <arsalmalik1920@gmail.com>
2025-11-18 23:19:21 +0400

    --

--- haraka/plugins/mongodb_store.js
+++ haraka/plugins/mongodb_store.js
@@ -16,18 +16,7 @@ exports.register = function () {
     .then((client) => {
       const db = client.db(dbName);
       collection = db.collection(collName);
-
       plugin.loginfo(`Mongo connected to ${url}, db ${dbName}, collection ${collName}`);
-
-      // ===========================
-      // TTL INDEX FOR TESTING: 1 MIN
-      // ===========================
-      collection.createIndex(
-        { receivedAt: 1 },
-        { expireAfterSeconds: 60 }   // 1 minute only
-      )
-      .then(() => plugin.loginfo("TTL index created for receivedAt (1 minute)."))
-      .catch((err) => plugin.logerror("TTL index error: " + err.message));
     })
     .catch((err) => {
       plugin.logerror("Mongo connection error: " + err.message);
@@ -36,6 +25,7 @@ exports.register = function () {
   plugin.loginfo("Registering hook: data_post");
   this.register_hook("data_post", "save_to_mongo");
 
+  // new, always mark queue as OK, so SMTP client gets 250
   plugin.loginfo("Registering hook: queue");
   this.register_hook("queue", "mark_queued_ok");
 };
@@ -82,7 +72,7 @@ exports.save_to_mongo = function (next, connection) {
         subject,
         headers: txn.header ? txn.header.headers_decoded : {},
         body,
-        receivedAt: new Date(),  // REQUIRED for TTL
+        receivedAt: new Date(),
         debug_uuid: txn.uuid,
         debug_get_data_call: getDataCount
       };
@@ -93,11 +83,11 @@ exports.save_to_mongo = function (next, connection) {
         .insertOne(doc)
         .then(() => {
           plugin.loginfo("Inserted email for " + rcpt_to + " with UUID " + txn.uuid);
-          next();
+          next(); // continue to queue hook
         })
         .catch((e) => {
           plugin.logerror("Mongo insert error: " + e.message);
-          next();
+          next(); // still continue, or you can next(constants.denysoft) if you want retry on DB error
         });
 
     } catch (e) {
@@ -107,6 +97,7 @@ exports.save_to_mongo = function (next, connection) {
   });
 };
 
+// this is called on the 'queue' hook, after data_post
 exports.mark_queued_ok = function (next, connection) {
   const plugin = this;
   plugin.loginfo("QUEUE hook, marking message as accepted");
